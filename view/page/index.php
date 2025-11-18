f7099705ae1669ef29b1a25f29abbc619762a612
marechalg <guewenmarechal@protonmail.com>
2025-11-19 00:43:09 +0100

    Add user session management and update page structure for index and play views

--- view/page/index.php
+++ view/page/index.php
@@ -1,3 +1,15 @@
+<?php
+
+require_once '../../model/User.php';
+session_start();
+
+if (!isset($_SESSION['user']) || !($_SESSION['user'] instanceof User)) {
+    header('Location: ./login.php');
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
--- view/page/play.php
+++ view/page/play.php
@@ -4,13 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wild West Saloon</title>
 
     <link rel="stylesheet" href="/view/style/styles.css">
 </head>
 
 <body>
-    
+    <header>
+
+    </header>
+
+    <main>
+        
+    </main>
 </body>
 
 </html>
